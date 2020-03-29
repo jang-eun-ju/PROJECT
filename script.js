3f4822e5ed1b6b3d58c24826fcc63605f55e58c7
eter2 <eter2hy@gmail.com>
2020-03-29 15:56:21 +0900

    Add background

---
+++ script.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById("canvas");
+
+var ctx = canvas.getContext("2d");
+
+ctx.strokeRect(0, 0, 100, 100);
+ctx.strokeRect(100, 0, 100, 100);
+ctx.strokeRect(200, 0, 100, 100);
+ctx.strokeRect(300, 0, 100, 100);
+ctx.strokeRect(0, 100, 100, 100);
+ctx.strokeRect(0, 200, 100, 100);
+ctx.strokeRect(0, 300, 100, 100);
+ctx.strokeRect(100, 300, 100, 100);
+ctx.strokeRect(200, 300, 100, 100);
+ctx.strokeRect(300, 300, 100, 100);
+ctx.strokeRect(300, 200, 100, 100);
+ctx.strokeRect(300, 100, 100, 100);
